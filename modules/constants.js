60c7cffe32aa04f1402e1b049087a5115bd77a6c
Denis Butyrskiy <d.butyrskiy@gmail.com>
2023-05-12 19:47:55 +0300

    added layout modal windows "settings, authorization, confirmation"

--- modules/constants.js
+++ modules/constants.js
@@ -1,6 +1,5 @@
 const messageForm = document.querySelector('.send-message__form'),
       messageInput = document.querySelector('.send-message__input'),
-      iMessageBox = document.querySelector('.i-message-box'),
-      iMessageText = document.getElementById('iMessage');
+      iMessageBox = document.querySelector('.i-message-box');
 
 export { messageForm, messageInput, iMessageBox };
